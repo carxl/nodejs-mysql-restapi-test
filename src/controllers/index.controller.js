b195b4be0ad8ac341a54f761f1c3fb989ffcd985
Andres <karxll@gmail.com>
2022-11-27 10:57:43 -0600

    Fix: Fixed error in index. Added index html

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,13 +1,3 @@
-import { pool } from "../db.js";
-
 export const getIndex = (req, res) => {
-  pool.query("SELECT 1 + 1 AS result", (error, result) => {
-    if (error) {
-      throw error;
-    } else {
-      console.log("result");
-      console.log(result);
-      res.json(result[0]);
-    }
-  });
+  res.sendFile("index.html", { root: "./src/templates" });
 };
